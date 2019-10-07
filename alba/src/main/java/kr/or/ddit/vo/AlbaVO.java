263625a65d68836f41c26609f551de3357fa18f2
mean.geee <ganada7522@naver.com>
2019-10-07 19:04:03 +0900

    DAO 수정 전

--- alba/src/main/java/kr/or/ddit/vo/AlbaVO.java
+++ alba/src/main/java/kr/or/ddit/vo/AlbaVO.java
@@ -1,11 +1,16 @@
 package kr.or.ddit.vo;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
-public class AlbaVO {
+@EqualsAndHashCode(of="al_id")
+@ToString(exclude= {"al_spec"})
+public class AlbaVO implements Serializable {
 	private String al_address ;
 	private int al_age     ;
 	private String al_btype   ;
@@ -18,7 +23,15 @@ public class AlbaVO {
 	private String al_name    ;
 	private String al_spec    ;
 	private String gr_code	  ;
-	private List<LicenseAlbaVO> licenseAlbaList;
 	private GradeVO grade;
 	private LicenseVO licencse;
+	
+	//요청 파라미터 바인드용
+	private String[] lic_codes;
+	
+	//데이터베이스 조회용
+	private String gr_name;
+	private List<LicenseAlbaVO> licenseAlbaList;
+
+	private String[] deleteLic_codes;
 }
