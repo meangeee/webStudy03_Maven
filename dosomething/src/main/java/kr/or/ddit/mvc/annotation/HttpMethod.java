5a097044f275445dfc077bed0142666325083dd5
MinJi <MinJi@DESKTOP-KUQ2L0E>
2019-10-16 01:26:42 +0900

    buyerList 마샬링 수정해야할듯

--- dosomething/src/main/java/kr/or/ddit/mvc/annotation/HttpMethod.java
+++
@@ -1,5 +0,0 @@
-package kr.or.ddit.mvc.annotation;
-
-public enum HttpMethod {
-	GET, POST, PUT, DELETE, HEAD, TRACE, OPTION
-}
--- dosomething/src/main/java/kr/or/ddit/mvc/annotation/IHandlerInvoker.java
+++
@@ -1,9 +0,0 @@
-package kr.or.ddit.mvc.annotation;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface IHandlerInvoker {
-	public String invokerHandler(URIMappingInfo mappingInfo, HttpServletRequest req, HttpServletResponse resp) throws Exception; //왜 String이라고요?
-	
-}
