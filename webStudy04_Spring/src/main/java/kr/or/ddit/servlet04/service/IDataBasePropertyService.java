0735a29fd0dcf7a8c71e6121c4ecb42910b4d789
mean.geee <ganada7522@naver.com>
2019-10-18 19:14:39 +0900

    추가 추가

--- webStudy04_Spring/src/main/java/kr/or/ddit/servlet04/service/IDataBasePropertyService.java
+++
@@ -1,7 +0,0 @@
-package kr.or.ddit.servlet04.service;
-
-import java.util.Map;
-
-public interface IDataBasePropertyService {
-	public void readAndLoggingDataBaseProperty(Map<String, Object> dataMap);
-}
